302b7a93c640c7bbec8acbad24c4ef82b790188b
OvercomingJunk <overcomingjunk@yandex.ru>
2022-08-01 15:46:18 +0300

    Парсинг https://career.habr.com/vacancies/java_developer?page=1 [#285210]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -11,19 +11,23 @@ import java.io.IOException;
 public class HabrCareerParse {
     private static final String SOURCE_LINK = "https://career.habr.com";
 
-    private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
+    private static int page = 1;
+
+    private static final String PAGE_LINK = String.format("%s/vacancies/java_developer?page=%d", SOURCE_LINK, page);
 
     public static void main(String[] args) throws IOException {
         Connection connection = Jsoup.connect(PAGE_LINK);
         Document document = connection.get();
         Elements rows = document.select(".vacancy-card__inner");
-        rows.forEach(row -> {
-            Element titleElement = row.select(".vacancy-card__title").first();
-            Element linkElement = titleElement.child(0);
-            String vacancyName = titleElement.text();
-            String dateTime = row.select(".vacancy-card__date").first().child(0).attr("datetime");
-            String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-            System.out.printf("%s %s %s %n", vacancyName, link, dateTime);
-        });
+        while (page++ <= 5) {
+            rows.forEach(row -> {
+                Element titleElement = row.select(".vacancy-card__title").first();
+                Element linkElement = titleElement.child(0);
+                String vacancyName = titleElement.text();
+                String dateTime = row.select(".vacancy-card__date").first().child(0).attr("datetime");
+                String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+                System.out.printf("%s %s %s %n", vacancyName, link, dateTime);
+            });
+        }
     }
 }
