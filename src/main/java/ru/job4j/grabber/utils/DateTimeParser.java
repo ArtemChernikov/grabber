b6066d07d71c5cd631df3f5e322f08357200f31f
OvercomingJunk <overcomingjunk@yandex.ru>
2022-08-01 15:43:08 +0300

    Преобразование даты [#289476]

--- src/main/java/ru/job4j/grabber/utils/DateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/DateTimeParser.java
@@ -2,6 +2,12 @@ package ru.job4j.grabber.utils;
 
 import java.time.LocalDateTime;
 
+/**
+ * Реализации этого интерфейса будут использоваться для преобразования даты и времени
+ *
+ * @author ARTEM CHERNIKOV
+ * @version 1.0
+ */
 public interface DateTimeParser {
     LocalDateTime parse(String parse);
 }
--- src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
@@ -4,9 +4,19 @@ import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * @author ARTEM CHERNIKOV
+ * @version 1.0
+ */
 public class HabrCareerDateTimeParser implements DateTimeParser {
+    /**
+     * Метод используется для преобразования даты из строки в объект
+     *
+     * @param parse - строка с датой и временем
+     * @return - возвращает {@link LocalDateTime}
+     */
     @Override
     public LocalDateTime parse(String parse) {
-        return ZonedDateTime.parse(parse, DateTimeFormatter.ISO_DATE_TIME).toLocalDateTime();
+        return ZonedDateTime.parse(parse).toLocalDateTime();
     }
 }
